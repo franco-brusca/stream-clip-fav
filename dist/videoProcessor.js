070baef0034f7b7037651bcfcb2b9a70f0f6a84a
unknown <Admin@PTECH-IOTDME.urbetrack.dc>
2024-06-15 17:38:36 -0300

    lowest video

--- dist/videoProcessor.js
+++ dist/videoProcessor.js
@@ -35,7 +35,7 @@ const downloadAndProcessVideo = (videoUrl, clipInfo, videoQuality, req, res) => 
         sendProgress('Starting video download and processing...');
         const info = yield ytdl_core_1.default.getInfo(videoUrl);
         let videos = ytdl_core_1.default.filterFormats(info.formats, 'videoonly');
-        let format = ytdl_core_1.default.chooseFormat(videos, { filter: format => format.qualityLabel === '480p' && format.container === 'mp4' });
+        let format = ytdl_core_1.default.chooseFormat(videos, { quality: "lowestvideo" });
         console.log(format);
         sendProgress('Video info retrieved.');
         const downloadAndClipVideo = new Promise((resolve, reject) => {
